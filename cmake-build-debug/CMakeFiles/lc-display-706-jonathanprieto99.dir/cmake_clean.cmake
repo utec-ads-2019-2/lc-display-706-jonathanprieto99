0f1f1ce59c64cd409c1781bfe852467aca88ccf5
Jonathan Prieto <jonathan.prieto@me.com>
2019-11-03 21:30:28 -0500

    Upload Files

---
+++ cmake-build-debug/CMakeFiles/lc-display-706-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lc-display-706-jonathanprieto99.dir/main.cpp.o"
+  "lc-display-706-jonathanprieto99"
+  "lc-display-706-jonathanprieto99.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lc-display-706-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
